31e05f242da3bc3c8694892cf7b0952c23a443b6
Mark Lopez <m@silvenga.com>
2025-09-01 12:56:10 -0500

    feat: added additional filter rules

--- merge.ps1
+++ merge.ps1
@@ -24,7 +24,16 @@ foreach ($list in $json.PSObject.Properties)
 
     Write-Host "Merging list '${listName}'..."
     $content = (Invoke-WebRequest -Uri $listUrl -UseBasicParsing).Content
-    $lines = $content -split '\r?\n' | Where-Object { -not [string]::IsNullOrWhiteSpace($_) -and -not $_.StartsWith('!') -and -not $_.Contains('+js') }
+    $lines = $content -split '\r?\n' | Where-Object {
+        -not [string]::IsNullOrWhiteSpace($_) `
+            -and -not $_.StartsWith('!') `
+            -and -not $_.StartsWith('[Ad') `
+            -and -not $_.Contains('+js') `
+            -and -not $_.Contains('$important') `
+            -and -not $_.Contains('$xhr') `
+            -and -not $_.Contains('$1p') `
+            -and -not $_.Contains('$3p')
+    }
     foreach ($line in $lines)
     {
         $merged.Add($line) | Out-Null
@@ -40,9 +49,10 @@ $header = @"
 ! Expires: 2 days (update frequency)
 ! License: https://www.gnu.org/licenses/gpl-3.0.html
 ! Homepage: https://github.com/Metrokoto/combined-abp-filters
+! URL: https://github.com/Metrokoto/combined-abp-filters/releases/download/latest/merged.txt
 "@
 
 $header | Out-File -FilePath $OutputPath -Encoding UTF8
-$merged | Out-File -FilePath $OutputPath -Encoding UTF8 -Append
+$merged | Sort-Object | Out-File -FilePath $OutputPath -Encoding UTF8 -Append
 
 Write-Host 'Done.'
